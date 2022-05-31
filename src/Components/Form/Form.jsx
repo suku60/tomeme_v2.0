d33ffa58c593f9260dc7af09624ada6bb7ba2ec7
Juan Manuel Stella <0x86jm14sMb94@gmail.com>
2022-06-01 00:35:00 +0200

    form basic structure created: displays a different return based on props. We'll use this component also for the form user will use to create posts in this media.

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import './Form.css';
 
 const Form = (props) => {
+  console.log(props.formType, typeof(props.formType))
 
   const [formType, setFormType] = useState(props.formType || undefined);
 
@@ -14,27 +15,72 @@ const Form = (props) => {
   useEffect(() => {
 
     setFormDisplay(props.displayFromParent);
+    setFormType(props.formType);
 
-  },[props.displayFromParent]);
+  },[props.displayFromParent, props.formType]);
+
+  console.log("antes del switch...", formType)
+
+switch(formType){
 
 
+  case "register":
   return (
     <div className='box_form centered_children' id="animReverseFade" style={{display: formDisplay}}>
       <div className="form_container" id="animItemComingFromBottom">
-        <div className="close_form_button centered_children" onClick={()=>{setFormDisplay("none")}}>X</div>
-          <form className='centered_children'>
-            <h1>Create your account</h1>
-            <label><b>Email</b></label>
-            <input type="text" name="email" placeholder="email" required/>
-            <input type="text" name="username" placeholder="username" required/>
-            <input type="password" name="password" placeholder="password" required/>
-            <input type="password" name="confirmation" placeholder="confirm your password" required/>
-            <p>By clicking Register, you'll agree you are over 18 and our <a href="https://blank.page/">Privacy Policy</a>.</p>
-            <button>Register</button>
-          </form>
+        <button className="close_form_button centered_children" onClick={()=>{setFormDisplay("none")}}>X</button>
+        <form className='centered_children'>
+          <h1>Create your account</h1>
+          <input type="text" name="email" placeholder="email" required/>
+          <input type="text" name="username" placeholder="username" required/>
+          <input type="password" name="password" placeholder="password" required/>
+          <input type="password" name="confirmation" placeholder="confirm your password" required/>
+          <p>By clicking Register, you'll agree you are over 18 and our <a href="https://blank.page/">Privacy Policy</a>.</p>
+          <button className='form_button' onClick={()=>{}}>Register</button>
+        </form>
       </div>
     </div>
   )
+  break;
+
+  case "login":
+    return (
+      <div className='box_form centered_children' id="animReverseFade" style={{display: formDisplay}}>
+        <div className="form_container" id="animItemComingFromBottom">
+          <button className="close_form_button centered_children" onClick={()=>{setFormDisplay("none")}}>X</button>
+          <form className='centered_children'>
+            <h1>Login with your account</h1>
+            <input type="text" name="username" placeholder="username" required/>
+            <input type="password" name="password" placeholder="password" required/>
+            <button className='form_button' onClick={()=>{}}>Login</button>
+          </form>
+        </div>
+      </div>
+    )
+  break;
+
+  // default is the same as register form now. We'll use this for
+  // making the form of "new meme post"
+  
+  default:
+  return (
+    <div className='box_form centered_children' id="animReverseFade" style={{display: formDisplay}}>
+      <div className="form_container" id="animItemComingFromBottom">
+        <button className="close_form_button centered_children" onClick={()=>{setFormDisplay("none")}}>X</button>
+        <form className='centered_children'>
+          <h1>Create your account</h1>
+          <input type="text" name="email" placeholder="email" required/>
+          <input type="text" name="username" placeholder="username" required/>
+          <input type="password" name="password" placeholder="password" required/>
+          <input type="password" name="confirmation" placeholder="confirm your password" required/>
+          <p>By clicking Register, you'll agree you are over 18 and our <a href="https://blank.page/">Privacy Policy</a>.</p>
+          <button className='form_button' onClick={()=>{}}>Register</button>
+        </form>
+      </div>
+    </div>
+  )
+  break;
+ }
 }
 
 export default Form;
--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -6,22 +6,31 @@ import Form from '../../Components/Form/Form';
 const Home = () => {
 
   const [formState, setFormState] = useState("none");
+  const [formType, setFormType] = useState(undefined);
 
-  const showForm = () => {
+  const showForm = (loginOrRegisterForm) => {
+
+    console.log("1")
+    setFormType(loginOrRegisterForm);
 
     if (!formState) {
       setFormState(true);
+    console.log("2")
+
     }else{
       setFormState(false);
+    console.log("3")
+
     }
   }
 
   return (
     <div className="box_basic_container home" id='animItemFallingFromTop'>
       <div className="board">
-        <Form displayFromParent={formState}/>
+        <Form displayFromParent={formState} formType={formType}/>
       </div>
-      <div className="temporaryBtn" onClick={()=>{showForm()}}></div>
+      <div className="temporaryBtn" onClick={()=>{showForm("login")}}>login</div>
+      <div className="temporaryBtn btn_register" onClick={()=>{showForm("register")}}>register</div>
     </div>
   )
 }
