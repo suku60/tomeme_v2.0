25f8a293f945dffc74be22f9f76c06f7c451df16
Juan Manuel Stella <0x86jm14sMb94@gmail.com>
2022-05-31 20:59:54 +0200

    Added basic inputs on form. Animations modified.

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -3,6 +3,8 @@ import './Form.css';
 
 const Form = (props) => {
 
+  const [formType, setFormType] = useState(props.formType || undefined);
+
   const [formDisplay, setFormDisplay] = useState("none");
 
   useEffect(() => {
@@ -14,11 +16,22 @@ const Form = (props) => {
     setFormDisplay(props.displayFromParent);
 
   },[props.displayFromParent]);
-  
+
+
   return (
-    <div className='box_form centered_children' id={"animItemComingFromBottom"} style={{display: formDisplay}}>
-      <div className="form">
+    <div className='box_form centered_children' id="animReverseFade" style={{display: formDisplay}}>
+      <div className="form_container" id="animItemComingFromBottom">
         <div className="close_form_button centered_children" onClick={()=>{setFormDisplay("none")}}>X</div>
+          <form className='centered_children'>
+            <h1>Create your account</h1>
+            <label><b>Email</b></label>
+            <input type="text" name="email" placeholder="email" required/>
+            <input type="text" name="username" placeholder="username" required/>
+            <input type="password" name="password" placeholder="password" required/>
+            <input type="password" name="confirmation" placeholder="confirm your password" required/>
+            <p>By clicking Register, you'll agree you are over 18 and our <a href="https://blank.page/">Privacy Policy</a>.</p>
+            <button>Register</button>
+          </form>
       </div>
     </div>
   )
