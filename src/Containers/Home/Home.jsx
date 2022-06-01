e805b170293bb4dea86465c333badf7d543024be
Juan Manuel Stella <0x86jm14sMb94@gmail.com>
2022-06-01 02:00:11 +0200

    Small style fixes for home. Imported main fonts for the app at app.css.

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -26,10 +26,15 @@ const Home = () => {
   }
 
   return (
-    <div className="box_basic_container home" id='animItemFallingFromTop'>
-        <Form displayFromParent={formState} formType={formType}/>
-      <div className="temporaryBtn" onClick={()=>{showForm("login")}}>login</div>
-      <div className="temporaryBtn btn_register" onClick={()=>{showForm("register")}}>register</div>
+    <div className="box_basic_container home" id='animReverseFade'>
+      <div className="background_sides"></div>
+      <div className="background_cover"></div>
+      <h1 className="welcome_text">Your daily dose of shitposting</h1>
+      <h2 className="welcome_subtext subtext_quote">- Albert Mandela</h2>
+      <h2 className="welcome_subtext subtext_join">Join the family</h2>
+      <Form displayFromParent={formState} formType={formType}/>
+      {/* <div className="temporaryBtn" onClick={()=>{showForm("login")}}>login</div> */}
+      {/* <div className="temporaryBtn btn_register" onClick={()=>{showForm("register")}}>register</div> */}
     </div>
   )
 }
